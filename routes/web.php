f4811ad82d7d2574bfda86f90afa8d89245266ed
angel ambrosio <nyels9@gmail.com>
2025-12-16 11:50:08 -0600

    instalacion de spatie

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+//otra forma de redirigir
+/*Route::get('/', function () {
+    return redirect()->route('login');
+});
+*/
+Auth::routes([
+    'register' => false,
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home')
+    ->middleware('auth');
