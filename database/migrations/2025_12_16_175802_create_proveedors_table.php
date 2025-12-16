1bfc6c2914b1b977f97270ba7a808a8fe46cad1b
angel ambrosio <nyels9@gmail.com>
2025-12-16 12:28:13 -0600

    creacion tabla proveedores

---
+++ database/migrations/2025_12_16_175802_create_proveedors_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proveedors', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('tipo_proveedor')->nullable();
+            $table->string('direccion')->nullable();
+            $table->string('codigo_postal')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('email')->nullable()->unique();
+            $table->string('ciudad')->nullable();
+            $table->string('persona_contacto')->nullable();
+            $table->string('telefono_contacto')->nullable();
+
+            $table->string('activo')->default('1');
+            $table->date('fecha_baja')->nullable();
+            $table->string('motivo_baja')->nullable();
+            $table->unsignedBigInteger('estado_id');
+            $table->foreign('estado_id')->references('id')->on('estados');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proveedors');
+    }
+};
